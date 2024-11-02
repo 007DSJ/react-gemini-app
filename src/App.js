4f2e44fdb16c1d5b4e993dc4efa2e728d2776b22
007DSJ <shinobigamerr565@gmail.com>
2024-11-02 12:50:13 +0530

    Base of frontend created with image saving in backend folder and display the selected file on the screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const App =()=> {
+
+  const [image, setImage] = useState(null)
+
+  const uploadImages = async (e) =>{
+    
+    const formData = new FormData()
+    formData.append('file',e.target.files[0])
+    setImage(e.target.files[0])
+
+    try {
+      const options = {
+        method: 'POST',
+        body: formData
+      }
+  
+      const response = await fetch('http://localhost:8000/upload', options)
+      const data = await response.json()
+
+    } catch (error) {
+      console.error(error)
+    }
+    
+  }
+
   return (
     <div className="app">
-
+      {image && <img src={URL.createObjectURL(image)}/>}
+      <label htmlFor="files">Upload an image</label>
+      <input onChange={uploadImages} id="files" accept="image/*" type="file" hidden />
     </div>
   );
 }
